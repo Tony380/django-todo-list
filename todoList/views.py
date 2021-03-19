9026be3195c5751470dc1b23ddfd16fac86482fd
Tony380 <tonygon2@yahoo.es>
2021-03-19 19:36:58 +0100

    setting up

--- todoList/views.py
+++ todoList/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TodoList
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index')
+
